16089c523ccb7648433ba440f68f72492e27b54d
Jose Luis De Avila Mercado <joseluisdeavila01@gmail.com>
2020-04-30 18:17:13 -0500

    Arreglado seccion de conocimientos

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ function mostrar(){
     let menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+function scroll(){
+    let menu = document.getElementById('navbar');
+    let letras = document.getElementsByClassName('link');
+    let scroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scroll>30){
+        menu.style.backgroundColor="white";
+        for(let i = 0; i<letras.length; i++){
+            letras[i].style.color="black";
+        }
+    }else{
+        menu.style.backgroundColor="transparent";
+        for(let i = 0; i<letras.length; i++){
+            letras[i].style.color="darkgray";
+        }
+    }
+}
