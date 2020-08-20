fac858f080cbf8f4849f1bdc283751b716e7a373
Jose De Avila <joseluisdeavila01@gmail.com>
2020-08-19 22:44:00 -0500

    Terminado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function mostrar(){
     let menu = document.getElementById('menu');
     menu.classList.toggle('active');
+    changeIcon();
 }
 
 function scroll(){
@@ -20,4 +21,49 @@ function scroll(){
             letras[i].classList.add('no-scroll');
         }
     }
+}
+
+function changeIcon(){
+    const icon = document.getElementById('btn-menu');
+    if(icon.innerHTML==="="){
+        icon.innerHTML = "X";
+    }else if(icon.innerHTML==="X"){
+        icon.innerHTML = "=";
+    }
+}
+
+function getURL(name,url){
+    if(!url){
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if(!results) return null;
+    if(!results[2]) return 'na';    
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+function showMessage(){
+    let success = document.referrer.indexOf(window.location.host)
+    console.log(success);
+    if(success==-1){
+        console.log("echo");
+        let success_msg = document.getElementById('msg-success');
+        success_msg.style.display="block";
+        let contact = document.getElementById('msg-success').getBoundingClientRect().top+document.getElementById('msg-success').getBoundingClientRect().bottom;
+        console.log(contact);
+    }
+}
+
+function showWindowSuccess(){
+    document.getElementById('alert-msg').classList.add("show");
+    let check = document.querySelector('.check-icon');
+    check.style.display = 'none';
+    setTimeout(() => {
+        check.style.display = 'block';
+    }, 10);
+}
+
+function HiddeWindowSuccess(){
+    document.getElementById('alert-msg').classList.remove("show");
 }
